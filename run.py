22dc57457583faa7eecb5a3d7f9f314ad1a776b0
osuossu8 <osuosuossu18@gmail.com>
2025-03-20 02:43:03 +0900

    fix

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 import torch
 from loguru import logger
+# import flash_attn_2_cuda as flash_attn_gpu
 
 
 def main():
+    logger.info(torch.__version__)
     logger.info(torch.cuda.is_available())
     logger.info(torch.cuda.current_device())
     logger.info(torch.cuda.device_count())
     logger.info(torch.cuda.get_device_name(0))
+    # logger.info(flash_attn_gpu.__version__)
 
 
 if __name__ == "__main__":
